696208da279390de944da1e5ee9680f7c8699c3b
monkhey_ <85946055+monkheyonepiece@users.noreply.github.com>
2023-02-24 15:39:49 +0100

    Update jvcplayer.user.js

--- jvcplayer.user.js
+++ jvcplayer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JvcPlayer
 // @namespace    https://github.com/monkheyonepiece/JvcPlayer
-// @version      1.0.0
+// @version      1.0.2
 // @downloadURL  https://github.com/monkheyonepiece/JvcPlayer/raw/main/jvcplayer.user.js
 // @updateURL    https://github.com/monkheyonepiece/JvcPlayer/raw/main/jvcplayer.user.js
 // @description  Intégration de vidéos YouTube et Youtube Short, Streamable, WebmShare, Tiktok, Vocaroo, IssouTV ou 4chan sur jeuxvideo.com
